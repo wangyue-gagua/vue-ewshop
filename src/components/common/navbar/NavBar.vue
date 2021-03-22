72bb23077184e125611440beb98f068baa768b7a
wangy <2503308794@qq.com>
2021-03-22 15:13:42 +0800

    add nav bar component

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-bar">
+      <div class='left'><slot name="left"></slot></div>
+      <div class='middle'><slot name="middle"></slot></div>
+      <div class='right'><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-      <h1>home</h1>
+    <nav-bar>
+      <template v-slot:middle>
+              <h1>home</h1>
+      </template>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'src/components/common/navbar/NavBar.vue'
 export default {
-
+  components: {
+    NavBar
+  }
 }
 </script>
 
